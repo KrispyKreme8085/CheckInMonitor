8be5d01f1c830a7a0c0d4ac082834713667a44fe
Karson Mellott <karsonmellott@gmail.com>
2025-12-10 11:36:14 -0700

    created room boxes

--- chckinmonitoradmin/src/app/page.tsx
+++ chckinmonitoradmin/src/app/page.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/header/header";
+import Room from "./components/roomBoxes/room";
+
+interface RoomProps {
+  name: string;
+  capacity: number;
+  currentOccupancy: number;
+}
 
 export default function Home() {
+
+  
+
   return (
     <div className={styles.page}>
       <Header />
+      <div>
+        <Room name="Conference Room A" capacity={20} currentOccupancy={5} />
+      </div>
     </div>
   );
 }
