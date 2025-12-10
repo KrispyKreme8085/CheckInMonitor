ca13342ebdd5caf2072c0744dc8b30c25d17d869
Karson Mellott <karsonmellott@gmail.com>
2025-12-10 11:40:40 -0700

    mapped rooms onto homepage

--- chckinmonitoradmin/src/app/page.tsx
+++ chckinmonitoradmin/src/app/page.tsx
@@ -11,13 +11,31 @@ interface RoomProps {
 
 export default function Home() {
 
-  
+  const rooms: RoomProps[] = [
+    { name: "Conference Room A", capacity: 20, currentOccupancy: 5 },
+    { name: "Meeting Room B", capacity: 15, currentOccupancy: 10 },
+    { name: "Lecture Hall C", capacity: 50, currentOccupancy: 25 },
+    { name: "Workshop Room D", capacity: 30, currentOccupancy: 15 },
+    { name: "Seminar Room E", capacity: 10, currentOccupancy: 2 },
+    { name: "Training Room F", capacity: 25, currentOccupancy: 12 },
+    { name: "Board Room G", capacity: 12, currentOccupancy: 6 },
+    { name: "Breakout Room H", capacity: 8, currentOccupancy: 3 },
+    { name: "Collaboration Room I", capacity: 18, currentOccupancy: 9 },
+    { name: "Quiet Room J", capacity: 5, currentOccupancy: 1 },
+  ];
 
   return (
     <div className={styles.page}>
       <Header />
-      <div>
-        <Room name="Conference Room A" capacity={20} currentOccupancy={5} />
+      <div className={styles.roomsContainer}>
+        {rooms.map((room, index) => (
+          <Room
+            key={index}
+            name={room.name}
+            capacity={room.capacity}
+            currentOccupancy={room.currentOccupancy}
+          />
+        ))}
       </div>
     </div>
   );
